d421290fac8bfd64b0b92e97b56da80124a9bab1
wooju94 <kom6381@naver.com>
2025-06-13 19:38:12 +0900

    selenium 예제

--- 11_selenium.py
+++ 11_selenium.py
@@ -15,10 +15,24 @@ def crawl_yanolja_reviews(name, url):
     soup = BeautifulSoup(driver.page_source)
 
     review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
+    review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div> div > div > div.css-1toaz2b > div > div.css-1ivchjf > p')
 
     for i in range(len(review_containers)):
-        print(i)
+        # print(i)
+        review_text = review_containers[i].find('p', class_='content-text').text
+        date = review_date[i].text
+        # print(review_text, date)
+        review_empty_stars = review_containers[i].find_all('path', {'fill-rule':'evenodd'})
+        # print(review_empty_stars)
+        stars = 5 - len(review_empty_stars)
+        review_dict = {
+            'review':review_text,
+            'star':stars,
+            'date':date
+        }
+        review_list.append(review_dict)
 
+    print(review_list)
     time.sleep(10)
 
 crawl_yanolja_reviews('라발스 호텔 부산', 'https://nol.yanolja.com/reviews/domestic/3020201')
